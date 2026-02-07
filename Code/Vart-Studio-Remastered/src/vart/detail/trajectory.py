ee82882fc0015257e7af81b97010bd1f15fba84f
KiraFlux <pret2titio1n@gmail.com>
2026-02-07 18:24:37 +0300

    Добавлен параметр tool_id

--- Code/Vart-Studio-Remastered/src/vart/detail/trajectory.py
+++ Code/Vart-Studio-Remastered/src/vart/detail/trajectory.py
@@ -16,16 +16,20 @@ class Trajectory:
             self,
             vertices: Sequence[vec2f],
             *,
+            tool_id: int = 1,
             is_looped: bool = False
     ):
-        self.on_change: Final[Subject[Trajectory]] = Subject()
+        self.on_geometry_change: Final[Subject[Trajectory]] = Subject()
+        self.on_tool_id_change: Final[Subject[int]] = Subject()
 
         self._vertices = vertices
+        self._tool_id = tool_id
         self._is_looped = is_looped
 
     def clone(self) -> Trajectory:
         return Trajectory(
             self._vertices,
+            tool_id=self._tool_id,
             is_looped=self._is_looped
         )
 
@@ -38,6 +42,10 @@ class Trajectory:
             result_x.append(x)
             result_y.append(y)
 
+        if self._is_looped:
+            result_x.append(result_x[0])
+            result_y.append(result_y[0])
+
         return result_x, result_y
 
     @property
@@ -48,11 +56,28 @@ class Trajectory:
     def set_vertices(self, v: Sequence[vec2f]):
         if self._vertices != v:
             self._vertices = v
-            self.on_change.notify(self)
+            self.on_geometry_change.notify(self)
 
     @vertices.setter
     def vertices(self, v):
         self.set_vertices(v)
+
+    @property
+    def tool_id(self):
+        """
+        Выбранный идентификатор инструмента
+        0 - Инструмент не выбран
+        """
+        return self._tool_id
+
+    def set_tool_id(self, tool_id: int):
+        if self._tool_id != tool_id:
+            self._tool_id = tool_id
+            self.on_tool_id_change.notify(tool_id)
+
+    @tool_id.setter
+    def tool_id(self, i):
+        self.set_tool_id(i)
 
     @property
     def is_looped(self):
@@ -66,4 +91,4 @@ class Trajectory:
     def is_looped(self, x):
         if self._is_looped != x:
             self._is_looped = x
-            self.on_change.notify(self)
+            self.on_geometry_change.notify(self)
